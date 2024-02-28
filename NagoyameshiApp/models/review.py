0683d84e1fbac1b80b5598f9a205224b26a7532c
ayaconsa <aya.consadole@gmail.com>
2024-02-28 15:40:03 +0900

    reviewモデルの修正

--- NagoyameshiApp/models/review.py
+++ NagoyameshiApp/models/review.py
@@ -4,17 +4,24 @@ from NagoyameshiApp.models.restaurant import Restaurant
 
 
 class Review(models.Model):
-    RATE = (
-        (1), (2), (3), (4), (5),
+    SCORE = (
+        (1, '★'), 
+        (2, '★★'), 
+        (3, '★★★'), 
+        (4, '★★★★'), 
+        (5, '★★★★★'),
     )
-    
+
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
     
-    rate = models.PositiveIntegerField(choices=RATE, max_length=1, default='', verbose_name="評価")
-    comment = models.TextField(max_length=300, default='', verbose_name="コメント")
+    score = models.PositiveSmallIntegerField(choices=SCORE, default='3', verbose_name="評価")
+    comment = models.TextField(blank=False, default='', verbose_name="コメント")
     created_at = models.DateTimeField(auto_now_add=True, verbose_name="登録日時")
     updated_at = models.DateTimeField(auto_now=True, verbose_name="更新日時")
     
     class Meta:
         app_label = 'NagoyameshiApp'
+        # 一人のユーザーが同じ店に対して2つ以上のレビューを書けないようにする
+        unique_together = ('restaurant', 'user')
+        
